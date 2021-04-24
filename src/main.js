376170b712a8cff290cb3cc32638f09f97855574
Renat Kutliyarov <Renat.Kutliyarov@gmail.com>
2021-04-24 15:47:23 +0300

    добавление редактирования

--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
 import {createNavigationComponent} from './view/navigationComponent.js';
 import {createFilterComponent} from './view/filterComponent.js';
-import {createTripComponent} from './view/tripComponent.js';
+import {createTripSummaryComponent} from './view/tripSummaryComponent.js';
 import {createCostComponent} from './view/costComponent.js';
+import {createSortingComponent} from './view/sortingComponent.js';
+import {createEditPoint} from './view/editPoint.js';
 
 const render = (container, component, place) => {
   container.insertAdjacentHTML(place, component);
 };
 
-const NUMBER_ENDPOINTS =3;
-const siteMainElement = document.querySelector('.trip-main__trip-controls');
+// Секция даты поездки и стоимости
+const siteTripMainElement  = document.querySelector('.trip-main');
+render(siteTripMainElement, createTripSummaryComponent(), 'afterbegin');
 
-const siteHeaderElement = siteMainElement.querySelector('.trip-controls__navigation');
+const siteTripSummaryElement = siteTripMainElement.querySelector('.trip-main__trip-info');
+render(siteTripSummaryElement, createCostComponent(), 'beforeend');
+
+// Меню и фильтры
+const siteMenuElement = document.querySelector('.trip-main__trip-controls');
+const siteHeaderElement = siteMenuElement.querySelector('.trip-controls__navigation');
 render(siteHeaderElement, createNavigationComponent(), 'beforeend');
 
-const siteFilterElement = siteMainElement.querySelector('.trip-controls__filters');
+const siteFilterElement = siteMenuElement.querySelector('.trip-controls__filters');
 render(siteFilterElement, createFilterComponent(), 'beforeend');
+
+const NUMBER_ENDPOINTS =3;
+
+const tripEventsElement = document.querySelector('.trip-events');
+render(tripEventsElement, createSortingComponent(), 'beforeend');
+
+render(tripEventsElement, createEditPoint(), 'beforeend');
